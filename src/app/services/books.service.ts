d961df078bf20a235002896d04d741844d85d342
Koimi YOVO <yovoedem33@gmail.com>
2020-09-09 18:39:34 +0200

    Book service methods

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,64 @@
-import { Injectable } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Subject } from 'rxjs';
+import * as firebase from 'firebase';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class BooksService {
 
+  books: Book[] = [];
+  bookSubject = new Subject<Book[]>();
+
   constructor() { }
+
+  emitBooks() {
+    this.bookSubject.next(this.books.slice());
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database()
+            .ref('/books')
+            .on('value', (data) => {
+              this.books = data.val() ? data.val() : [];
+              this.emitBooks();
+            });
+  }
+
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id)
+                .once('value')
+                .then(
+                  (data) => {
+                    resolve(data);
+                  },
+                  (error) => {
+                    reject(error);
+                  }
+                );
+      }
+    );
+  }
+
+  createNewBook(book: Book) {
+    this.books.push(book);
+    this.saveBooks();
+    this.emitBooks();
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if (bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
