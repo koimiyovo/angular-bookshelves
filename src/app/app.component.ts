8fdac5d117f29226ff701c16f0e9bd18ec6639c4
Koimi YOVO <yovoedem33@gmail.com>
2020-09-09 17:27:48 +0200

    Firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyC_p24Dy4BhqfTF3jSKTYk54RmkGbdUczM',
+      authDomain: 'bookshelves-e1257.firebaseapp.com',
+      databaseURL: 'https://bookshelves-e1257.firebaseio.com',
+      projectId: 'bookshelves-e1257',
+      storageBucket: 'bookshelves-e1257.appspot.com',
+      messagingSenderId: '1098291978455',
+      appId: '1:1098291978455:web:47657754ca3d55d6039092'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
