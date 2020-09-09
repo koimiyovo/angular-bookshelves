2b13383dd100bedb975016b2dc51ded915b5c005
Koimi YOVO <yovoedem33@gmail.com>
2020-09-09 18:58:56 +0200

    Implement book list component

--- src/app/pages/book-list/book-list.component.ts
+++ src/app/pages/book-list/book-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { Subscription } from 'rxjs';
+import { BooksService } from 'src/app/services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription;
+
+  constructor(private booksService: BooksService, private router: Router) { }
 
   ngOnInit(): void {
+    this.booksSubscription = this.booksService.bookSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
   }
 
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
+  }
 }
