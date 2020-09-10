a262ef05ae59c099115491da483b69472c8c44e3
Koimi YOVO <yovoedem33@gmail.com>
2020-09-10 08:59:38 +0200

    Add synopsis field the book form et single-book view

--- src/app/pages/book-list/book-form/book-form.component.ts
+++ src/app/pages/book-list/book-form/book-form.component.ts
@@ -24,14 +24,17 @@ export class BookFormComponent implements OnInit {
   initForm() {
     this.bookForm = this.formBuilder.group({
       title: ['', Validators.required],
-      author: ['', Validators.required]
+      author: ['', Validators.required],
+      synopsis: ''
     });
   }
 
   onSaveBook() {
     const title = this.bookForm.get('title').value;
     const author = this.bookForm.get('author').value;
+    const synopsis = this.bookForm.get('synopsis').value;
     const newBook = new Book(title, author);
+    newBook.synopsis = synopsis;
     this.booksService.createNewBook(newBook);
     this.router.navigate(['/books']);
   }
